1abff2b6cceff392e8868d8361e3406ee734a213
muyun <wenlzhao@gmail.com>
2013-04-14 18:12:59 +0800

    correct the function based on more clear neighbor definition

--- 1155011105_assign3.py
+++ 1155011105_assign3.py
@@ -4,7 +4,7 @@
 # which is about a simple recommender system using the user-based collaborative filtering
 # Todo: using class is better
 # ***************************************************************************************
-from __future__ import division
+#from __future__ import division
 import re, numpy
 from sets import Set
 
@@ -86,8 +86,10 @@ def calPearsonr(list1,list2):
 
     from scipy.stats import pearsonr
     p = pearsonr(list1,list2)[0]
+   
+    print "%f" % p
     if str(p) == "nan":
-        p = 0
+        p = 0.0
     #print "%f" % p
 
     return p
@@ -103,29 +105,33 @@ def similarity(ratings,u1, u2):
 
     # get common list
     nolist = getUnion(nolist1, nolist2)
-    
-    # get the u1's list, which only include the common items between u1 and u2
-    list1 = [item for index, item in enumerate(ratings[u1]) if index not in nolist]
-    list2 = [item for index, item in enumerate(ratings[u2]) if index not in nolist]
+    #if there is common list
+    if len(nolist) != 0:
+        # get the u1's list, which only include the common items between u1 and u2
+        list1 = [item for index, item in enumerate(ratings[u1]) if index not in nolist]
+        list2 = [item for index, item in enumerate(ratings[u2]) if index not in nolist]
 
-    # calculate pearson correlation
-    # meet the function parameters, so bad ...
-    pearsonr = calPearsonr(list1, list2)
-    print "%f" % pearsonr
-    return  pearsonr
+        # calculate pearson correlation
+        # meet the function parameters, so bad ...
+        pearson = calPearsonr(list1, list2)
+        #print "%f" % pearson
+        return  pearson
+    else:
+        pearson = "unknown"
+        print "u1 %d and u2 %d arenot neighbors with each other" % u1 % u2
 
 def getNeighbors(ratings, u, N, b):
     # neighbors's definition:
     #1. They have rated items that X has rated
     #2. They have rated Y
     #so, we should get a sorted list of N users based on their similarity(distance) to u
-    #also, the neighbor's (Assume u3 here) items include u's items and b (b is Y)
+    #also, the neighbor's (Assume u3 here) items include b (b is Y here) which X doesn't rate
 
-    #similarity
+    #similarities stores the similarity information between user and u
     similarities = []
     # get -1 index for u
-    nolist0 = []
-    nolist0 = getNoRelatedItems(ratings, u)
+    #nolist0 = []
+    #nolist0 = getNoRelatedItems(ratings, u)
 
     print "Find the neighbors of a given user:"
     # len(ratings) is the number of users, because ratings is an array and begin from 0
@@ -133,37 +139,30 @@ def getNeighbors(ratings, u, N, b):
     user = 0
     while user < len(ratings):
         if user != u:
-            nolist1 = []
-            nolist1 = getNoRelatedItems(ratings, user)
-            # because this is no related items, so it is contary
-            # test whether nolist1 is subset of nolist
-            # which equals that current user is superset of u
-            if Set(nolist1).issubset(Set(nolist0)):
-                # get common list
-                nolist = getUnion(nolist1, nolist0)
-                # get the current user's list, which only include the common items between current user and required u
-                list1 = [item for index, item in enumerate(ratings[user]) if index not in nolist]
-                list2 = [item for index, item in enumerate(ratings[u]) if index not in nolist]
-
-                # calculate pearson correlation
-                # meet the function parameters, so bad ...
-                pearson = calPearsonr(list1, list2)
-
+            #nolist1 = []
+            #nolist1 = getNoRelatedItems(ratings, user)
+            #user rated book b while u rated book b 
+            if ratings[u][b] == -1 and ratings[user][b] != -1:
+                #calculate pearson correlation between current user and u
+                pearson = similarity(ratings, user, u)
+ 
                 similarities.append((pearson, user))
                 
         user += 1        
         
     s = sorted(similarities,key = lambda similarities: similarities[0],reverse=True)
-
+    #print s
+ 
     similarities = s[:N]
-        
-    userList = []
-    for similarity in similarities:
-        userList.append(similarity[1])
-            #print "userid:%d" % similarity[1]
-            
-    return userList  
     
+    userList = []
+    for fields in similarities:
+        userList.append(fields[1])
+        #print "userid:%d" % fields[1]
+     
+    print userList      
+    return userList  
+ 
 
 def predict(ratings, u, b, neighbors):
     #Since the neighbours of u have been given
@@ -172,85 +171,64 @@ def predict(ratings, u, b, neighbors):
     # filter neighbors because some of them might not have rated the particular book b
     # which means related item's value is not -1
 
-    # get the -1's index list of u
-    nolist0 = []
-    nolist0 = getNoRelatedItems(ratings, u)
-    
     # store the pearson and user for b
     newPearson = []
-    # store the indices in neighbors but not in u
-    allList = []
         
     print "Predict the rating for a given book and neighbors:"            
     
     for neighbor in neighbors:
-        if ratings[neighbor][b] != -1:
+        # only when book b is in the neighbors but not in u
+        if ratings[neighbor][b] != -1 and ratings[u][b] == -1:
             # also, the neighbor should meet the neighbor's definition
-            # which means that the neighbor user should be superset of u
-            # that's to say, -1 items should be subset of u's
-            nolist1 = []
-            nolist1 = getNoRelatedItems(ratings, neighbor)
-            if Set(nolist1).issubset(Set(nolist0)):
-                # current indices in current neighbors but not in u
-                list = []
-                # get common list
-                nolist = getDifference(nolist0, nolist1)
-                #get the indices which are not included by u
-                # this list stores the indices in current neighbor and not in u
-                # in this list, we only care index b,so
-                list = [item for index, item in enumerate(ratings[neighbor]) if index in nolist and index == b]
-                allList = allList + list
-             
-                if len(list) != 0:
-                    #only when current neighbor rated book b and required user u didn't ( in nolist)
-                    # then we calculate the pearson between current neighbor and u                
-                    # the same code
-                    # get common list
-                    nolist = getUnion(nolist1, nolist0)
-                    # get the current user's list, which only include the common items between current user and required u
-                    list1 = [item for index, item in enumerate(ratings[neighbor]) if index not in nolist]
-                    list2 = [item for index, item in enumerate(ratings[u]) if index not in nolist]
+            # which means that the neighbor user should include common items with u
+            # that's to say, The union of their not-readed items(-1) shouldnot be null
 
-                    # calculate pearson correlation
-                    pearson = calPearsonr(list1, list2)
-                    newPearson.append((pearson,neighbor))
-                    
-                else:
-                    continue
-                    
-    #only when book b is in the neighbors but not in u
-    if len(allList) != 0:                
-        # then calculate pearson for each neighbor        
-        # similar to k-nearest
-        value = 0
-
-        #have to add a new list for sum
-        sumList = []
-        for fields in newPearson:
-            pearson = fields[0]
-            sumList.append(pearson)
-
-        #get the sum
-        pearsonSum = sum(sumList)
-        
-        #        print "pearsonSum %f" % pearsonSum
-        for fields in newPearson:
-            pearson = fields[0]
-            user = fields[1]
-            if pearsonSum == 0.0:
-                pearsonValue = 1
+            # calculate pearson correlation
+            pearson = similarity(ratings,neighbor, u)
+            # if they are not neighbor
+            if pearson == "unknown":
+                continue
             else:
-                pearsonValue = pearson / pearsonSum
-           
-            value += ratings[user][b] * pearsonValue
-            #print "value1: %f" % value
+                newPearson.append([neighbor, pearson])
+                    
+                
+    # then calculate sum of each neighbor's pearson        
+    # similar to k-nearest
     
-        return value
+    #predicted rating
+    value = 0.0
+    #sum of pearson
+    pearsonSum = 0.0
+
+    userList = []
+    sumList = []
+    print newPearson
+    for fields in newPearson:
+        # prepare for getting the number of neighbor
+        # here we can't use len(neighbor) as the number of neighbor,
+        # because some user might not be u's neighbor
+        userList.append(fields[0])
+        sumList.append(fields[1])
+
+    #get the sum
+    pearsonSum = sum(sumList)
+    #print "pearsonSum %f" % pearsonSum
         
-    # or return u's rating
-    else:
-        #print "%f" % ratings[u][b]
-        return ratings[u][b]
+    for fields in newPearson:
+        user = fields[0]
+        pearson = fields[1]
+        if pearsonSum == 0.0:
+            #calculate the rated average of neighbors
+            pearsonValue = pearson / float(len(userList))
+        else:
+            pearsonValue = pearson / pearsonSum
+           
+        value += ratings[user][b] * pearsonValue
+        
+    print "value1: %f" % value
+    
+    return value
+        
           
 if  __name__ == "__main__":
     #Global variable
@@ -280,16 +258,58 @@ if  __name__ == "__main__":
     #find the neighbors of some user
     N = []
     item = 0
-    getNeighbors(ratings, 0, 5, item)
+    getNeighbors(ratings, 0, 2, 2)
     
     # predict the rating that some user would give to a book
     # list neighbors is a list of indices of the nighbors of user u
-    neighbors = [4,5,6,7,8,9]
-    predict(ratings, 3, 4, neighbors)
-  
+    neighbors = [4,5,1]
+    predict(ratings, 0, 2, neighbors)
+    
+    #****************************************************************
     # evaluation
     print "Perform evaluation:"
-    # get the neighbors
+    # mean rating
+    # make use of other than user himslef 
+    user = 0
+    # record each i
+    i = 0
+    # rating for first two book
+    b0 = 0.0
+    b1 = 0.0
+    
+    meanRating= []
+    # store the mean rating
+    # for each user
+    while user < len(ratings):
+        # at each line
+        while i < len(ratings):
+            #apart from user himself
+            if user != i:
+                #book 0
+                b0 += ratings[i][0]
+                b1 += ratings[i][1]
+                
+            i += 1
+        meanRating.append([b0,b1,user])
+        
+        user += 1
+    print "meanRating:"
+    print meanRating
+    #predicted rating using predict function 
+    # get the new ratings matrix newratings
+    # remove the first two items list on martix ratings according to the instruction
+    i = 0
+    line = []
+    # store the new ratings
+    newratings = []
+    while i< len(ratings):
+        line = ratings[i]
+        # remove the first two column
+        del line[0:2]
+        newratings.append(line)
+        i += 1    
+    
+    
     #userlist store the list of neighbor when given a user
     userList05  = []
     userList010 = []
@@ -302,18 +322,6 @@ if  __name__ == "__main__":
     predictedRating15 = []
     predictedRating110 = []
 
-    # remove the first two items list on martix ratings
-    i = 0
-    line = []
-    # store the new ratings
-    newratings = []
-    while i< len(ratings):
-        line = ratings[i]
-        # remove the first two column
-        del line[0:2]
-        newratings.append(line)
-        i += 1
-    
     i = 0
     while i<len(newratings):
         #for book 0, size of neighbor is 5
@@ -358,50 +366,22 @@ if  __name__ == "__main__":
             #predictedRating110.append(-1)
 
         i += 1
-    # mean rating
+
+    # compute the RMSE(root mean squared error)for the three types of predictions
+     #for mean method
+    meanDist0 = meanDist1 = 0.0
+    # [b0,b1,user]
+    for fields in meanRating:
+        b0 = fields[0]
+        b1 = fields[1]
+        user = fields[2]
+
+        meanDist0 += (ratings[user][0] - b0) ** 2
+        meanDist1 += (ratings[user][1] - b1) ** 2
+ 
+     #for predictd function   
     # for book 0 based on the size of neighbors is 5
-    distance05 = distance010= distance15 = distance110= 0
-
-    for fields in predictedRating05:
-        user = fields[1]
-        value = fields[0]
-
-        distance05 += abs(ratings[user][0] - value)
-
-    # 10
-    for fields in predictedRating010:
-        user = fields[1]
-        value = fields[0]
-        distance010 += abs(ratings[user][0] - value)
-
-   # for book 1
-    for fields in predictedRating15:
-        user = fields[1]
-        value = fields[0]
-        distance15 += abs(ratings[user][1] - value)
-
-
-    for fields in predictedRating110:
-        user = fields[1]
-        value = fields[0]
-        distance110 += abs(ratings[user][1] - value)
-
-    # get the value
-    meanValue05 = distance05 / len(newratings)
-    meanValue010 = distance010 / len(newratings)
-    
-    meanValue15 = distance15 / len(newratings)
-    meanValue110 = distance110 / len(newratings)
-
-    print "mean rating of first book with the size of neighborhood is 5: %f" % meanValue05
-    print "mean rating of first book with the size of neighborhood is 10: %f" % meanValue010
-    print "mean rating of second book with the size of neighborhood is 5: %f" % meanValue15
-    print "mean rating of second book with the size of neighborhood is 10: %f" % meanValue110
-
-
-    # RMSE(root mean squared error)
-        # for book 0 based on the size of neighbors is 5
-    distance05 = distance010= distance15 = distance110= 0
+    distance05 = distance010= distance15 = distance110= 0.0
     #book 0
     for fields in predictedRating05:
         user = fields[1]
@@ -429,12 +409,19 @@ if  __name__ == "__main__":
 
     # get the value
     import math
+    # mean method
+    rmseValue0 = math.sqrt(meanDist0 / len(ratings))
+    rmseValue1 = math.sqrt(meanDist1 / len(ratings))
+    
+    #predicted function
     rmseValue05 = math.sqrt(distance05 / len(ratings))
     rmseValue010 = math.sqrt(distance010 / len(ratings))
     
     rmseValue15 = math.sqrt(distance15 / len(ratings))
     rmseValue110 = math.sqrt(distance110 / len(ratings))
-
+    
+    print "RMSE rating of first book with mean rating: %f" % rmseValue0
+    print "RMSE rating of second book with mean rating: %f" % rmseValue1
     print "RMSE rating of first book with the size of neighborhood is 5: %f" % rmseValue05
     print "RMSE rating of first book with the size of neighborhood is 10: %f" % rmseValue010
     print "RMSE rating of second book with the size of neighborhood is 5: %f" % rmseValue15
@@ -442,13 +429,10 @@ if  __name__ == "__main__":
 
     #the mean and RMSE for the four types of predictions
 '''
-mean rating of first book with the size of neighborhood is 5: 0.000000
-mean rating of first book with the size of neighborhood is 10: 0.000000
-mean rating of second book with the size of neighborhood is 5: 0.400000
-mean rating of second book with the size of neighborhood is 10: 0.400000
-RMSE rating of first book with the size of neighborhood is 5: 0.000000
-RMSE rating of first book with the size of neighborhood is 10: 0.000000
-RMSE rating of second book with the size of neighborhood is 5: 0.894427
-RMSE rating of second book with the size of neighborhood is 10: 0.894427
+RMSE rating of first book with mean rating: 19.241617
+RMSE rating of second book with mean rating: 51.362143
+RMSE rating of first book with the size of neighborhood is 5: 1.457574
+RMSE rating of first book with the size of neighborhood is 10: 3.990158
+RMSE rating of second book with the size of neighborhood is 5: 1.289845
+RMSE rating of second book with the size of neighborhood is 10: 3.829169
 '''
-    
